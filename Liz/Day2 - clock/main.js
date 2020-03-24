817897cb9c63c4390e938b3c7b1507a38ec0fc99
Liz Wang <lizwang@LizdeMacBook-Pro.local>
2020-03-24 11:28:48 +0800

    modified UI

--- Liz/Day2 - clock/main.js
+++ Liz/Day2 - clock/main.js
@@ -2,22 +2,26 @@ const secondHand = document.querySelector('.second-hand');
 const minHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-function setDate (){
+const secondNumber = document.querySelector('.secondNumber');
+const minNumber = document.querySelector('.minNumber');
+const hourNumber = document.querySelector('.hourNumber');
+
+const setDate = () => {
   const now = new Date()
-  
+
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60 ) * 360 );
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  secondNumber.innerHTML = seconds;
   // console.log(seconds)
   const mins = now.getMinutes();
   const minutesDegrees = ((mins / 60 ) * 360 );
   minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  minNumber.innerHTML = mins;
 
   const hours = now.getHours();
   const hoursDegrees = ((hours / 12 ) * 360 );
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
+  hourNumber.innerHTML = hours;
 }
-
-
 setInterval(setDate,1000);
