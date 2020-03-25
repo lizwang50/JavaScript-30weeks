f86d936429b6b24733fc9d77c64858239a202de2
etrex kuo <et284vu065k3@gmail.com>
2020-03-26 02:49:05 +0800

    add Etrex's clock

---
+++ Etrex/Day2 - clock/main.js
@@ -0,0 +1,98 @@
+function update(dot, speed){
+  dot.renderX += (dot.x - dot.renderX) * speed;
+  dot.renderY += (dot.y - dot.renderY) * speed;
+  dot.style.top = dot.renderX + "px";
+  dot.style.left = dot.renderY + "px";
+}
+
+function noise(){
+  return Math.random() * 40 - 20;
+}
+
+function updateDots(){
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach((dot)=>{
+    update(dot, 0.05)
+  })
+}
+
+function updateMs(ms){
+  const dot = document.querySelector("#ms .dot");
+  dot.x = Math.cos(-2 * Math.PI * ms / 1000) * 300 + 300;
+  dot.y = Math.sin(-2 * Math.PI * ms / 1000) * 300 + 300;
+  update(dot, 1)
+}
+
+function updateSeconds(seconds){
+  const dots = document.querySelectorAll("#second .dot");
+  const length = 250;
+  const gap = length / (dots.length-1);
+  const angle = -2 * Math.PI * seconds / 60;
+  for(let i = 0; i < dots.length ; i++){
+    let dot = dots[i];
+    dot.x = Math.cos(angle) * i * gap + 300 + noise();
+    dot.y = Math.sin(angle) * i * gap + 300 + noise();
+  }
+}
+
+function updateMinutes(minutes){
+  const dots = document.querySelectorAll("#minute .dot");
+  const length = 200;
+  const gap = length / (dots.length-1);
+  const angle = -2 * Math.PI * minutes / 60;
+  for(let i = 0; i < dots.length ; i++){
+    let dot = dots[i];
+    dot.x = Math.cos(angle) * i * gap + 300 + noise();
+    dot.y = Math.sin(angle) * i * gap + 300 + noise();
+  }
+}
+
+function updateHours(hours){
+  const dots = document.querySelectorAll("#hour .dot");
+  const length = 150;
+  const gap = length / (dots.length-1);
+  const angle = -2 * Math.PI * hours / 12;
+  for(let i = 0; i < dots.length ; i++){
+    let dot = dots[i];
+    dot.x = Math.cos(angle) * i * gap + 300 + noise();
+    dot.y = Math.sin(angle) * i * gap + 300 + noise();
+  }
+}
+
+function updateTime(time){
+  const h1 = document.querySelector("#time");
+  h1.innerText = time;
+} 
+
+function format(number){
+  if(number > 10)
+    return number;
+  return "0" + number;
+}
+
+function onEnterFrame(){
+  const now = new Date();
+  const ms = now.getMilliseconds();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  updateTime(format(hours) + ":" + format(minutes) + ":" + format(seconds));
+  updateHours(hours + minutes / 60);
+  updateMinutes(minutes);
+  updateSeconds(seconds);
+  updateMs(ms);
+  updateDots();
+}
+
+function initDots(){
+  const dots = document.querySelectorAll(".dot");
+  dots.forEach((dot)=>{
+    dot.renderX = Math.random()*600;
+    dot.renderY = Math.random()*600;
+    dot.x = Math.random()*600;
+    dot.y = Math.random()*600;    
+  })
+}
+
+initDots();
+setInterval(onEnterFrame, 10);
