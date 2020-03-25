48128d2491e86745fa5f1aefb1a8831c702da927
purple1007 <purple1007@gmail.com>
2020-03-26 00:59:08 +0800

    update layout

--- Debby/Day-2-clock/javascript/all.js
+++ Debby/Day-2-clock/javascript/all.js
@@ -2,43 +2,26 @@ const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
+
+
 function setDate() {
-  // 抓到今日
   const now = new Date();
-  // console.log(now);
 
-  // 抓到秒
-  const seconds = now.getSeconds();
-  // console.log(second);
+  const minseconds = now.getMilliseconds() / 1000 ;
+  const seconds = now.getSeconds() + minseconds;  // 讓秒數取到毫秒
+  // console.log(seconds);
+  const mins = now.getMinutes();
+  const hours = now.getHours();
 
-  // 計算一秒選轉的度數。
+  const newMins = mins + seconds / 60;
+
   const secondsDegrees = ((seconds / 60) * 360) + 90;
-  // console.log(secondsDegrees);
-  // 為什麼要加 90 ?
-  // 沒有加 90 度的話，指針會從橫向 0° 開始旋轉，因為 css 的指針是寫 height : 2px / with: 50%;
-
-
-  // 將計算出來的度數，套到 CSS。
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  // 抓到分鐘;
-  const mins = now.getMinutes();
-  // console.log(mins);
-
-  const minsDegrees = ((mins/60) * 360) + ((seconds/60) * 6) + 90;
-  // (mins / 60) * 360) -> 分針在每分鐘會轉的度數。
-  // ((seconds/60) * 6) -> 分針在每一秒會轉的度數。
-
-  // console.log(minsDegrees);
-  // console.log(mins/60);
+  const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-  // 抓到小時;
-  const hours = now.getHours();
-  const hoursDegrees = ((hours/12) * 360) + ((mins/60) * 30) + 90;
-  // (hours / 12) * 360) -> 時針在每小時會轉的度數。
-  // ((mins / 60)* 30) -> 時針在每分鐘會轉的度數。
-  console.log((mins/60) * 30);
+  const hoursDegrees = ((hours / 12) * 360) + ((mins / 60) * 30) + 90;
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
