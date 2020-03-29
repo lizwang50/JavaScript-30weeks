d12eca8d0d1bd1507b66bd6ad6accbd7109e5802
Cindy Liu <cindyliu923@gmail.com>
2020-03-29 22:34:33 +0800

    Fix cindy clock url

--- cindy/Day2-clock/main.js
+++ cindy/Day2-clock/main.js
@@ -33,7 +33,7 @@ const setDate = () => {
 }
 
 chickHead.addEventListener('click', () => {
-  audio.src = encodeURI(`https://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q=${time.innerText}&tl=en-gb`);
+  audio.src = encodeURI(`http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q=${time.innerText}&tl=en-gb`);
   audio.autoplay = 'autoplay';
 });
 
